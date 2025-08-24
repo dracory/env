8510ad64010b2f93010f444f074ca62ae7e7d6cd
Milan Lesichkov <lesichkovm@gmail.com>
2025-08-24 08:05:10 +0100

    Update constants.go

--- constants.go
+++ constants.go
@@ -11,13 +11,14 @@ const (
 	// - "1" (as well as any other positive number)
 	// - 0.01 (as well as any other positive float)
 	// - "yes", "Yes", "YES"
-	TrueValues = "true,True,TRUE,T,t,1,yes,Yes,YES"
+	// - "on", "On", "ON"
+	TrueValues = "true,True,TRUE,T,t,1,yes,Yes,YES,on,On,ON"
 
 	// False values
 	// - "false", "False", "FALSE"
 	// - "F", "f"
 	// - "0", (as well as any other negative number)
 	// - 0.00 (as well as any other negative float)
-	// - "no", "No", "NO"
-	FalseValues = "false,False,FALSE,F,f,0,no,No,NO"
+	// - "off", "Off", "OFF"
+	FalseValues = "false,False,FALSE,F,f,0,no,No,NO,off,Off,OFF"
 )
